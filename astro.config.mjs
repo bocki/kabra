255afb5b67def5ef75e9b2d22186fda7eeadbe28
Florian <bocki@users.noreply.github.com>
2023-12-09 19:32:42 +0100

    added changes for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://katherinabraschel.com',
   integrations: [tailwind(), alpinejs()]
 });
