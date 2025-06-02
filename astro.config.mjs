fb1d10d6d67e59bef461667877a55244311f54c6
Florian <bocki@users.noreply.github.com>
2025-06-02 13:21:43 +0200

    prepare for yaml input

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://katherinabraschel.com',
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
